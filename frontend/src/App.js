7e7680d4ac7e0a992147d46979e1d7697681ba31
NhanNguyen <nhan281297@gmail.com>
2020-11-01 10:07:42 +0700

    Build Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-  
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import Product from '../components/Product';
+import data from '../data';
 
 function HomeScreen(props) {
   return (
-    <div>
-      
+    <div className="row center">
+      {data.products.map((product) => (
+        <Product key={product._id} product={product}></Product>
+      ))}
     </div>
   );
 }
