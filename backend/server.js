54419e03e2e4459b865f6d60a7cd883678d6c370
nhannguyen <nhan281297@gmail.com>
2020-11-29 14:37:57 +0700

    lecture checkout wizard

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -12,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 /*
@@ -36,16 +37,22 @@ app.use('/api/users', userRouter);
 
 app.use('/api/products', productRouter);
 
+app.use('/api/orders', orderRouter);
+
+app.get('/api/config/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 app.get('/', (req, res) => {
-  res.send('Server is ready');
+    res.send('Server is ready');
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+    res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
-  console.log(`Server at http://localhost:${port}`);
+    console.log(`Server at http://localhost:${port}`);
 });
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,43 +1,54 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import {
-  productDetailsReducer,
-  productListReducer,
+    productDetailsReducer,
+    productListReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
-  userRegisterReducer,
-  userSigninReducer,
+    userRegisterReducer,
+    userSigninReducer,
 } from './reducers/userReducers';
-
+import {
+    orderCreateReducer,
+    orderDetailsReducer,
+    orderPayReducer,
+} from './reducers/orderReducers';
 
 const initialState = {
-  userSignin: {
-    userInfo: localStorage.getItem('userInfo')
-      ? JSON.parse(localStorage.getItem('userInfo'))
-      : null,
-  },
-  cart: {
-    cartItems: localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
-      : [],
-  },
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')
+            ? JSON.parse(localStorage.getItem('userInfo'))
+            : null,
+    },
+    cart: {
+        cartItems: localStorage.getItem("cartItems")
+            ? JSON.parse(localStorage.getItem("cartItems"))
+            : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+            ? JSON.parse(localStorage.getItem('shippingAddress'))
+            : {},
+        paymentMethod: 'PayPal',
+    },
 };
 
 const reducer = combineReducers({
-  productDetails: productDetailsReducer,
-  userRegister: userRegisterReducer,
-  productList: productListReducer,
-  userSignin: userSigninReducer,
-  cart: cartReducer,
+    cart: cartReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    productDetails: productDetailsReducer,
+    productList: productListReducer,
+    userRegister: userRegisterReducer,
+    userSignin: userSigninReducer,
+    orderPay: orderPayReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
+    reducer,
+    initialState,
+    composeEnhancer(applyMiddleware(thunk))
 );
 
 export default store;
